35c1558bd3e2fb9adc8446cc33aacdb5fd60350a
Caio Henrique <caiohenriquesk@gmail.com>
2021-01-16 23:48:14 -0300

    Fix bug: Intl not found no Android

--- front-mobile/src/OrderCard/index.tsx
+++ front-mobile/src/OrderCard/index.tsx
@@ -4,6 +4,8 @@ import { Order } from '../types';
 import dayjs from 'dayjs';
 import 'dayjs/locale/pt-br';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import "intl";
+import "intl/locale-data/jsonp/pt-BR";
 
 dayjs.locale('pt-br');
 dayjs.extend(relativeTime);
@@ -31,7 +33,7 @@ function OrderCard({ order }: Props) {
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.orderName}>Pedido {order.id}</Text>
-        <Text style={styles.orderPrice}>R$ {order.total}</Text>
+        <Text style={styles.orderPrice}>{formatPrice(order.total)}</Text>
       </View>
       <Text style={styles.text}>{dateFromNow(order.moment)}</Text>
       <View style={styles.productsList}>
